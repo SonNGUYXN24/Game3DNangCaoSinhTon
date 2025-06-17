18b43226917c7bbf90408d3d2a0cd3353af1dbbe
HuyXo <169118171+HuyXo@users.noreply.github.com>
2025-06-17 22:50:09 +0700

    1

---
+++ Assets/QuangHuy/Scipts/BearSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BearSpawner : MonoBehaviour
+{
+    public GameObject bearPrefab; // Prefab con gấu
+    public int numberOfBears = 5; // Số lượng gấu muốn sinh
+    public float spawnRange = 20f; // Phạm vi sinh
+
+    void Start()
+    {
+        for (int i = 0; i < numberOfBears; i++)
+        {
+            SpawnBear();
+        }
+    }
+
+    void SpawnBear()
+    {
+        // Vị trí ngẫu nhiên quanh vị trí của Spawner
+        Vector3 randomPos = new Vector3(
+            transform.position.x + Random.Range(-spawnRange, spawnRange),
+            transform.position.y,
+            transform.position.z + Random.Range(-spawnRange, spawnRange)
+        );
+
+        Instantiate(bearPrefab, randomPos, Quaternion.identity);
+    }
+}
